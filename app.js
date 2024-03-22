229758c0f04211bc5bf72612dee7f816be1a68af
Davi Eler Magalhães <davi.eler.magalhaes@gmail.com>
2024-03-21 21:52:57 -0500

    Text animation, reset button, adjusting css

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
-let startButton = document.querySelector(".btn-start") 
-let secondsDiv = document.querySelector(".seconds")
-let minutesDiv = document.querySelector(".minutes")
-const totalSecondsReset = parseInt(minutesDiv.textContent) * 60
+let startButton = document.querySelector(".btn-start");
+let secondsDiv = document.querySelector(".seconds");
+let minutesDiv = document.querySelector(".minutes");
+let appMessage = document.querySelector(".app-message")
+const totalSecondsReset = parseInt(minutesDiv.textContent) * 60;
 
-let state = true
-let myInterval
-startButton.addEventListener("click", function(){
+let state = true;
+let myInterval;
 
-    if(state) {
-        state = false
-        let totalSeconds = parseInt(minutesDiv.textContent) * 60
-
+startButton.addEventListener("click", function() {
+    startButton.textContent = "RESET"
+    appMessage.style.opacity = 0;
+    if (state) {
+        state = false;
+        let totalSeconds = parseInt(minutesDiv.textContent) * 60;
+        updateDisplay(totalSeconds); //update timer immediately to 24:59 so it feels more responsive
         function countdown() {
-            if (totalSeconds>0){
-                totalSeconds--
-                let currentSeconds = totalSeconds % 60
-
-                secondsDiv.textContent = currentSeconds < 10 ? "0" + String(currentSeconds) : String(currentSeconds)
-                minutesDiv.textContent = String(Math.floor(totalSeconds / 60))     
-            }else{
-                clearInterval(myInterval);
-                totalSeconds = totalSecondsReset
-                minutesDiv.textContent = String(Math.floor(totalSeconds / 60))  
-                state = true
-            }  
+            if (totalSeconds > 0) { //time running
+                totalSeconds--;
+                updateDisplay(totalSeconds);
+            } else { // time completed
+                new Audio("/imgs/bell.wav").play();
+                resetTimer();
+            }
         }
+        countdown();
         myInterval = setInterval(countdown, 1000);
-    }else{
-        alert("Timer has already started")
+    } else {
+        resetTimer();
     }
+});
 
-})
+function resetTimer() {
+    startButton.textContent = "START";
+    clearInterval(myInterval);
+    totalSeconds = totalSecondsReset;
+    updateDisplay(totalSeconds);
+    state = true;
+    appMessage.style.opacity = 1;
+}
+
+function updateDisplay(totalSeconds) {
+    let currentSeconds = totalSeconds % 60;
+    secondsDiv.textContent = currentSeconds < 10 ? "0" + String(currentSeconds) : String(currentSeconds);
+    minutesDiv.textContent = String(Math.floor(totalSeconds / 60));
+}
